82be886a1a3ab0931df9c7c8d1881bb7cf9d98f8
Rodrigo <rodri45ESP@gmail.com>
2022-03-01 15:01:17 +0100

    Bcolors class moved to separate file

--- diceGame/pig_classes.py
+++ diceGame/pig_classes.py
@@ -1,59 +1,9 @@
+
 """Classes module for Dice Pig game."""
 
 from random import randint
 import pickle
 import should_roll as prob
-
-
-class Player():
-    """Player class with name, score, tries and wins atributes"""
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
-        self.won = False
-        if(name in ['RODRI45Z', 'HIVA', 'YANA']):  # CHEAT CODE
-            self.score = 99
-        self.turn_score = 0
-
-    def sum_turn_score(self):
-        """Sums turn score to total score and sets score to 0"""
-        self.score += self.turn_score
-        self.turn_score = 0
-
-    def change_name(self, name):
-        """changes the player name"""
-        self.name = name
-
-
-class Dice():
-    """Dice class with roll argument"""
-    def __init__(self):
-        self.roll = 0
-
-    def roll_dice(self, print_dice):
-        """sets a random number between 1-6 into roll variable and
-        if Print Dice true, it prints an ASCII drawing of the dice.
-        """
-        with open('./diceGame/dice_drawings.pickle', 'rb') as handle:
-            dice_face_dic = pickle.load(handle)
-            self.roll = randint(1, 6)
-        if print_dice:
-            print('\n' + dice_face_dic[self.roll] + '\n')
-
-
-class Bcolors:
-    """Colors to print in cmd"""
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    NOT_UNDERLINED = '\033[24m'
-    RESET = '\u001b[0m'
 
 
 class Scoreboard():
@@ -172,10 +122,10 @@ valid option\n!!!!!!')
         print(f"{Bcolors.OKBLUE}Your score this turn is {player.turn_score}")
         player.sum_turn_score()
         print(f"{player.name}'s Total score this turn is {player.score}")
-
         return player
 
     def computerTurn(self, computer, player):
+        """THIS IS THE METHOD FOR THE COMPUTER TURN """
         dice = Dice()
         while prob.should_roll(computer.score, player.score, computer.turn_score):
             dice.roll_dice(False)

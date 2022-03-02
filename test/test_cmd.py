94b273861d6d281ebb685332d1d398c0156d94f0
Rodrigo Lara <rodri45esp@gmail.com>
2022-03-02 21:31:22 +0100

    test cmd updte

--- test/test_cmd.py
+++ test/test_cmd.py
@@ -2,7 +2,6 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from diceGame import cmd_game
-from diceGame import Game
 import unittest
 from unittest import mock
 import cmd
@@ -15,8 +14,24 @@ class TestCmd(unittest.TestCase):
         '''Test  PigGame class'''
         cmd_g = cmd_game.PigGame(cmd.Cmd)
         self.assertIsInstance(cmd_g, cmd_game.PigGame)
+
+    @mock.patch('Game.Game.playerVsMachine')
+    def test_do_Game(self, mock):
+        """Test if do_play calls playerVsMachine func."""
+        cmd_g = cmd_game.PigGame(cmd.Cmd)
+        cmd_g.do_play('1')
+        self.assertTrue(mock.called)
+
+    @mock.patch('Game.Game.playerVsPlayer')
+    def test_do_Game2(self, mock):
+        """Test if do_play calls playerVsMachine func."""
+        cmd_g = cmd_game.PigGame(cmd.Cmd)
+        cmd_g.do_play('2')
+        self.assertTrue(mock.called)
     
-    # @mock.patch.object(Game.Game, "playerVsMachine")
-    # def test_do_Game(self, mock):
-    #     cmd_g = cmd_game.PigGame(cmd.Cmd)
-    #     mock.assert_called()
+    # Add test_do_game3. Mock print when we input wrong argument.
+    # Add test_scoreboard, follow example in test_do_Game,
+    # check if scoreboard is called.
+    # Check do bye print and returning true.
+    # Check do rule print
+
--- test/test_scoreboard.py
+++ test/test_scoreboard.py
@@ -1,16 +1,16 @@
 """Dice Unittest"""
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from diceGame import Scoreboard
 import unittest
 from unittest.mock import patch
 import pickle
 
-class TestDice(unittest.TestCase):
-    """Dice testing class"""
+class TestScoreboard(unittest.TestCase):
+    """Scoreboard testing class"""
 
     def test_Dice(self):
-        '''Test Dice class'''
+        '''Test Scoreboard class'''
         scoreboard = Scoreboard.Scoreboard()
         self.assertIsInstance(scoreboard, Scoreboard.Scoreboard)
     

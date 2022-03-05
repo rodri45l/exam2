adcd870d0f5fe4dacab7a770c23de9e6e43edb14
roya <hivamoghaddam78@gmail.com>
2022-03-05 08:19:01 +0100

    game.py tests

--- test/test_cmd.py
+++ test/test_cmd.py
@@ -16,19 +16,43 @@ class TestCmd(unittest.TestCase):
         self.assertIsInstance(cmd_g, cmd_game.PigGame)
 
     @mock.patch('Game.Game.playerVsMachine')
-    def test_do_Game(self, mock):
+    def test_do_play1(self, mock):
         """Test if do_play calls playerVsMachine func."""
         cmd_g = cmd_game.PigGame(cmd.Cmd)
         cmd_g.do_play('1')
         self.assertTrue(mock.called)
 
     @mock.patch('Game.Game.playerVsPlayer')
-    def test_do_Game2(self, mock):
+    def test_do_play2(self, mock):
         """Test if do_play calls playerVsMachine func."""
         cmd_g = cmd_game.PigGame(cmd.Cmd)
         cmd_g.do_play('2')
         self.assertTrue(mock.called)
     
+    @mock.patch("builtins.print")
+    def test_do_play3(self, mock):
+        """ Test if we pass an argument other thatn 1 or 2 """
+        cmd_g = cmd_game.PigGame(cmd.Cmd)
+        cmd_g.do_play('3')
+        str = "please press 1 or 2 (you pressed: 3)"
+        mock.assert_called_with(str)
+
+    @mock.patch("Scoreboard.Scoreboard.print_scorebard")
+    def test_do_scoreboard(self, mock):
+        cmd_g = cmd_game.PigGame(cmd.Cmd)
+        cmd_g.do_scoreboard("")
+        self.assertTrue(mock.called)
+
+    @mock.patch("builtins.print")   
+    def test_do_rule(self, mock):
+        """ Test if print function as required """
+        cmd_g = cmd_game.PigGame(cmd.Cmd)
+        cmd_g.do_rule('3')
+        str = "LetCol.HEADERDIVIDERLetCol.OKCYAN\ nIn this game wins the "
+        mock.assert_called_with(str)
+
+
+
     # Add test_do_game3. Mock print when we input wrong argument.
     # Add test_scoreboard, follow example in test_do_Game,
     # check if scoreboard is called.
--- test/test_game.py
+++ test/test_game.py
@@ -13,4 +13,20 @@ class TestDice(unittest.TestCase):
         '''Test Dice class'''
         game = Game.Game()
         self.assertIsInstance(game, Game.Game)
-    
+
+class Game(unittest.TestCase):
+    """ Game Testing class """
+    @mock.patch("builtins.print")
+    def test_showOptionMenu(self, mock):
+        """Test show option menu """
+        game = Game.game()
+        game.Game("7")
+        str = "Enter your choice: 7"
+        mock.assert_called_with(str)
+
+    def test_createPlayer(self):
+        """ Test creating player """
+        game = Game.game()
+        game.Game("1")
+        str = "Please Player n \ enter your name: 1 "
+        mock.assert_called_with(str)
